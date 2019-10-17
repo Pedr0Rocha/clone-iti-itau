16629fa925dbcd32a3aec151165eca836e7a90e5
pedr0rocha <pedr0_r@hotmail.com>
2019-10-16 21:44:38 -0300

    App Colors

---
+++ lib/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static Color background = new Color(0xFF2b384b);
+
+  static Color orange = new Color(0xFFee6d0f);
+  static Color pink = new Color(0xFFfe5689);
+
+}
