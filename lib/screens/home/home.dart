2797d7436ab5aa6d0778ce30d7fefc8636580cd3
pedr0rocha <pedr0_r@hotmail.com>
2019-10-16 22:49:52 -0300

    User avatar and assets

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -28,8 +28,49 @@ class _HomePageState extends State<HomePage> {
       child: Container(
         height: MediaQuery.of(context).size.height,
         color: AppColors.background,
-        child: Container(),
+        child: Column(
+          children: <Widget>[
+            _userAvatar(),
+            _infoBalance(),
+            _carouselActions(),
+          ],
+        ),
       ),
     );
+  }
+
+  Widget _userAvatar() {
+    return Stack(
+      alignment: Alignment.bottomRight,
+      children: <Widget>[
+        _roundImageWithBorder(imagePath: "assets/img/user.png", imageSize: 80.0, borderWidth: 3.0),
+        _roundImageWithBorder(imagePath: "assets/img/qrcode.png", imageSize: 25.0, borderWidth: 3.0),
+      ],
+    );
+  }
+
+  Widget _roundImageWithBorder({ String imagePath, double imageSize, double borderWidth }) {
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            color: AppColors.orange,
+            borderRadius: BorderRadius.circular(imageSize),
+          ),
+          width: imageSize + borderWidth,
+          height: imageSize + borderWidth,
+        ),
+        ClipOval(child: Image.asset(imagePath, width: imageSize, height: imageSize, fit: BoxFit.fill,)),
+      ],
+    );
+  }
+
+  Widget _infoBalance() {
+    return Container();
+  }
+
+  Widget _carouselActions() {
+    return Container();
   }
 }
